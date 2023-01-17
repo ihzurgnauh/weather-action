99312519136642bbfed153c1f19ff8fff9eb5743
ihzurgnauh <48985431+ihzurgnauh@users.noreply.github.com>
2023-01-17 17:45:15 +0800

    Update weather.sh

--- weather.sh
+++ weather.sh
@@ -11,4 +11,4 @@ curl \
   -H "Accept-Language: $LANGUAGE" \
   -H "User-Agent: $UA" \
   -o result.html \  
-  https://wttr.in/$CITY?format=%20%l:+%C+%c+%t%0a+%E6%97%A5%E5%87%BA%EF%BC%9A%S+-+%E6%97%A5%E8%90%BD%EF%BC%9A%s%0a+%E9%BB%8E%E6%98%8E%EF%BC%9A%D+-+%E6%AD%A3%E5%8D%88%EF%BC%9A%z+-+%E9%BB%84%E6%98%8F%EF%BC%9A%d%0a+%E6%9C%88%E7%9B%B8%EF%BC%9A%m+-+%E6%9C%88%E4%BA%AE%E6%97%A5%EF%BC%9A%M%0a+%E9%99%8D%E6%B0%B4%EF%BC%9A%p%0a+%E6%B0%94%E5%8E%8B%EF%BC%9A%P%0a+Current%20time%EF%BC%9A%T+%20+Local%20timezone%EF%BC%9A%Z
+  https://wttr.in/$CITY?format=3
